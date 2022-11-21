4513660eec665cb0ff91424efc4d849fb072632a
Prajwal C S <117572354+Prajwal-Rathod@users.noreply.github.com>
2022-11-21 18:57:18 +0530

    password

---
+++ 1234.py
@@ -0,0 +1,95 @@
+# # # deliting string
+# # value = "hello world"
+# # print(value)
+# # del value
+# # # # print(value)
+
+# # # # concatinating string
+# # value1 = " hello"
+# # value2 =  ' world'
+# # print(value2 *3+ value1*3)
+
+# # # print("prajwal",end=" ")
+# # # print("c s")\
+
+# # # cheking if a substrin is prasent in a string  
+# # value = "hello world"
+# # if "Hello" .lower() in value:
+# #     print("hello is prasent in astring y\n")
+# # elif "hello" not in value:
+# #     print("hello is not prasent in the string")
+
+# # # showing the use of raw string in python
+# # value = "are u learning python ? yes\no"
+# # print(value)
+
+# # value_modified = r" are u learning python? yes\no 
+
+
+# # # count. index. find. lower and upper
+# # value = "Hello world"
+# # print(value.count("He"))  # countr method
+
+# # print(value.find("H"))
+
+# # print(value.index("H"))
+
+# # print(value.lower())
+
+# # print(value.upper())
+
+
+# # itirating through string
+# value = "hello world"
+
+# # for i in value:
+# #     print(i)
+# # for i in range(len(value)):
+# # #     print(value[i])
+
+# # # to find the length of a string
+# a = "prajwal"
+# count = 0
+# for i in a:
+#     count += 1
+# # print(count)
+
+
+# # # case=1 : start and end index
+# # value = "tinker"
+
+# # # # start and end
+# # # print(value[0:5])
+
+# # # # start index
+# # # print(value[6:])
+
+# # # # end index
+# # # print(value[:5])
+
+# # # # start and enfd with step
+
+# # print(value[5::-2])
+
+# i = 10 
+# i /= 2
+# print(i)
+
+
+# number= range(5)
+# print(number)
+
+# marks = [100,23,56]
+
+# i = 0
+# while i < len(marks):
+#     print(marks[i])
+#     i = i + 1 
+ 
+
+# # # to find the length of a string
+a = "prajwal"
+count = 0
+for i in a:
+    count += 1
+print(count)
